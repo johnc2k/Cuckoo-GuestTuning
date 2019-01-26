fce8150d09c63058b8ddaa3dfb28829dd52d7f15
John Cobb <johnc@nobytes.com>
2019-01-26 15:06:32 +0000

    no message

---
+++ Win10/Win10.bat
@@ -0,0 +1,6 @@
+:: Win10: Disable ISATAP, Teredo, and 6to4
+netsh int ipv6 isatap set state disabled
+netsh int ipv6 6to4 set state disabled
+netsh interface teredo set state disable
+:: Win10: Disable Windows Firewall
+netsh advfirewall set allprofiles state off
